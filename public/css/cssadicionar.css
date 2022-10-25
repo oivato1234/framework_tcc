49d3591541220685df71b777d8deb6e2b4605a45
Yara Garcia <yara.garcia@etec.sp.gov.br>
2022-10-24 21:39:14 -0300

    inclusão dos arquivos css

---
+++ public/css/cssadicionar.css
@@ -0,0 +1,34 @@
+<style>
+            *{
+  margin:8px;
+  padding:4px;
+
+ }
+
+.tamanhoimg{
+        width: 50px;
+        height: 50px;
+    }
+
+    .tbadicionar{
+        position: relative;
+        right: 120px;
+    }
+
+    .carrinho{
+        font-family:Arial, Helvetica, sans-serif;
+  background: url(https://acegif.com/wp-content/uploads/loading-47.gif);
+  background-size: 100%;
+  color: #DCDCDC;  
+    }
+    
+    .voltar{
+position: relative;
+top: 1;
+text-decoration: inline;
+color: black;
+right: 10px;
+
+}
+    
+</style>
---
+++ public/css/csshome.css
@@ -0,0 +1,54 @@
+<style>
+        *{
+  margin:8px;
+  padding:4px;
+
+ }
+    .bodyhome{
+        
+   background: url(https://images.alphacoders.com/437/437363.jpg);
+   background-size: 125%;
+   background-repeat: no-repeat;
+       font-family:Arial, Helvetica, sans-serif;
+       text-align: center;
+       color: #DCDCDC;
+
+
+    }
+    .box1{
+transform: translate(-50,-50);
+background-color: rgba(0, 0, 0, 0);
+padding: 40px;
+border-radius: 20px;
+
+
+}
+.login{
+  text-decoration: none;
+  color: white;
+  border: 3px solid dodgerblue;
+  border-radius: 25px;
+  padding: 20px;
+  position: relative;
+  text-align: center;
+  
+}
+
+.login:hover{
+background-color: #00FF7F;
+}
+
+
+
+
+/*.carousel-item.active{
+  left:35em;
+  position:relative;
+  width: 100em;
+  height: 200px;
+  margin: 0 auto;
+}*/
+
+
+
+</style>
