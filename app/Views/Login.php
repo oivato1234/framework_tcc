4d273efd709917f51fb34f3b3e90f4e132474895
PBOsx <pablo.souza76@etec.sp.gov.br>
2022-10-27 20:48:22 -0300

    login controller, layout

--- app/Views/Login.php
+++ app/Views/Login.php
@@ -1,105 +1,9 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tela de Login</title>
-    <style>
-        body {
-   font-family: Arial, Helvetica, sans-serif;
-   background-image:url(https://img5.goodfon.com/wallpaper/nbig/6/17/strelka-sinii-fon-linii-geometriia-goluboi.jpg);
-        }
-
-        h2 {
-            position: absolute;
-            left: 35%;
-            top: 11px;
-        }
-
-            
-        .tela-login {
-         background-color: rgba(0, 0, 0, 0.9);
-         position: absolute;  
-         top: 50%;
-         left: 50%;
-         transform: translate(-50%, -50%);
-         padding: 80px;
-         border-radius: 15px;
-         color: white; 
-        }
-
-        input{
-            padding: 15px;
-            border: none;
-            outline: none;
-            font-size: 15px;
-
-        }
-
-        .inputSubmit{
-            background-color: dodgerblue;
-            border: none;
-            padding: 15px;
-            width: 100%;
-            border-radius: 10px;
-            color: white;
-            font-size: 15px;
-            
-        }
-
-        .inputSubmit:hover{
-            background-color: deepskyblue;
-            cursor: pointer;
-
-
-        }
-
-        .cadastro{
-            background-color: #8B8989;
-            border: none;
-            padding: 5px;
-            width: 100%;
-            border-radius: 20px;
-            color: white;
-            font-size: 15px;
-
-        }
-
-        .voltar{
-position: fixed;
-top: 2;
-background-color: rgba(0, 0, 0, 5);
-border-radius: 20px;
-text-decoration: none;
-color: #DCDCDC;
-border: 2px solid dodgerblue;
-padding: 8px;
-
-}
-
-.voltar:hover{
-    background-color: #32CD32;
-}
-
-.esqueci{
-    position: relative;
-  text-align: center;  
-  text-decoration: underline;
-color: white; 
-
- 
-}
-     
-
-    </style>
-</head>
-<body> 
-    <?php
-   
-           include 'bancocadastro.php';
-           include "configbanco.php";
-           
+<?php
+     $this->extend('layout/Layout');
+        // include 'bancocadastro.php';
+        // include "configbanco.php";
+     $this->endsection();
+     $this->section('conteudo');          
 ?>
  <a class= "voltar" href="home.php"><b>Voltar</b></a>
     <div class="tela-login">
@@ -116,11 +20,6 @@ color: white;
     </form> <br>
       <a class="esqueci" a href=""> <h4>Esqueci minha senha</h4> </a>   
     </div>
-   
-
-  
-   
-
-
-</body>
-</html>
+<?php
+    $this->endsection();
+?>
