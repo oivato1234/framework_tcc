2defc1f9e0aa7080661b096aa18e616d35a06000
DihMendonca <diemenson.mendonca@etec.sp.gov.br>
2022-11-07 22:21:22 -0300

    css

--- app/Views/suporteajuda.php
+++ app/Views/suporteajuda.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo base_url('css/css_pecas.css')?>">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <title>Suporte Ajuda</title>
 </head>
 <body>
@@ -13,7 +16,7 @@
 <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="pecas/placa-mae.png" class="d-block w-25"> <br><br>
+      <img src="public/pecas/placa-mae.png" class="d-block w-25"> <br><br>
       <p class="texto">Placa Mãe e onde vai seus respectivos componentes</p>
     </div>
   </div>
